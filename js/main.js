93d0e201c9787ab0753cf856b2d6e23553068ca3
Josias Matschulat <josiasmatschulat@outlook.com>
2024-10-30 13:32:15 -0300

    Correct version number

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 "use strict";
 
-const VERSION = "2024-10-10";
+const VERSION = "2024-10-30";
 
 const config_storage = new LocalStorageHandler("pcsetviz");
 const config_visible_data = new LocalStorageHandler("pcsetviz-visible-data");
