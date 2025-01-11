e0a6af5724809e675ec3f320f32d174684b05200
Josias Matschulat <josiasmatschulat@outlook.com>
2025-01-11 19:18:47 -0300

    implemented playScale() and a more efficient oscillator model

--- js/audio.js
+++ js/audio.js
@@ -20,47 +20,37 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 const PitchPlayer = {
     
-    gain : 0.1,
+    gain : 0.5,
     fade_in : 0.05,
     fade_out : 0.05,
     
     /** @type {AudioContext?} */
     audio_ctx : null,
+    /** @type {Array.<OscillatorNode[]>} */
     oscillators : Array(12).fill(null),
+    /** @type {GainNode[]} */
     gains : Array(12).fill(null),
     base_freqs : [
         32.7, 34.65, 36.71, 38.89, 20.6, 21.83, 
         23.12, 24.5, 25.96, 27.5, 29.14, 30.87
     ],
 
-    createOscillator(pc) {
-        const base_freq = this.base_freqs[pc];
-        this.oscillators[pc] = [];
-        const now = this.audio_ctx.currentTime;
-        for ( let f = base_freq; f < 20000; f*=2 ) {
-            const osc = this.audio_ctx.createOscillator();
-            osc.type = "sine";
-            osc.frequency.setValueAtTime(f, now); 
-            osc.connect(this.gains[pc]);
-            this.oscillators[pc].push(osc);
-        }
-    },
-
-    destroyOscillator(pc) {
-        for ( const osc of this.oscillators[pc] ) {
-            osc.disconnect(this.gains[pc]);
-        }
-        this.oscillators[pc] = null;
-    },
-
-    createOscillators() {
-        for ( let pc = 0; pc < 12; pc++ )
-            this.createOscillator(pc);
+    createSingleOscillator(pc) {
+        const real = new Float32Array(257).fill(0.0);
+        const imag = new Float32Array(257).fill(0.0);
+        for ( let i = 1; i <= 512; i*=2 ) real[i] = 1.0;
+        const osc = this.audio_ctx.createOscillator();
+        osc.setPeriodicWave(
+            this.audio_ctx.createPeriodicWave(real, imag, { disableNormalization: false})
+        );
+        osc.frequency.setValueAtTime(this.base_freqs[pc], this.audio_ctx.currentTime);
+        osc.connect(this.gains[pc]);
+        this.oscillators[pc] = [osc];
     },
 
     initialize() {
         this.audio_ctx = new AudioContext();
-        const lpf = new BiquadFilterNode(this.audio_ctx, {type: "lowpass", Q: 0.4, frequency: 2000});
+        const lpf = new BiquadFilterNode(this.audio_ctx, {type: "lowpass", Q: 0.6, frequency: 1800});
         const hpf = new BiquadFilterNode(this.audio_ctx, {type: "highpass", Q: 1, frequency: 130});
         lpf.connect(this.audio_ctx.destination);
         hpf.connect(lpf);
@@ -68,7 +58,6 @@ const PitchPlayer = {
             const g = this.audio_ctx.createGain();
             g.gain.value = 0;
             g.connect(hpf);
-            // g.connect(this.audio_ctx.destination);
             this.gains[pc] = g;
         }
     },
@@ -94,7 +83,7 @@ const PitchPlayer = {
             this.initialize();
         const pc = pitch%12;
         if ( !this.oscillators[pc] ) {
-            this.createOscillator(pc);
+            this.createSingleOscillator(pc);
             this.setGradualGainChange(pc, 0, this.gain, this.fade_in, delay);
             for ( const osc of this.oscillators[pc] )
                 osc.start();
@@ -109,10 +98,22 @@ const PitchPlayer = {
             const end_time = this.setGradualGainChange(pc, this.gain, 0, this.fade_out, delay);
             for ( const osc of this.oscillators[pc] ) {
                 osc.addEventListener("ended", (e) => { e.target.disconnect(this.gains[pc]); });
+                
                 osc.stop(end_time);
             }
             this.oscillators[pc] = null;
         }
-    }
+    },
+
+    /**
+     * @param {Number[]} pitches 
+     * @param {Number} pitch_duration 
+     */
+    playScale(pitches, pitch_duration) {
+        for ( let i = 0; i < pitches.length; i++ ) {
+            const delay = i * pitch_duration;
+            this.playPitch(pitches[i], pitch_duration, delay);
+        }
+    },
 
 }
